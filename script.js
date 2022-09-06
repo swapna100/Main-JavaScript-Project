41583ec54002a05851a2abc3bf6cbb6212943c3c
swapna samuel <33215403+swapna100@users.noreply.github.com>
2022-09-06 09:09:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,75 +7,68 @@ let compScoretotal = 0; //total computer score
 // function containing the selection of computer
 function computerPlay()
     {
-    let items = ['Rock', 'Paper', 'Scissors'];
-    let compChoice = items[Math.floor(Math.random()* items.length)];
-    return compChoice; //return the selection of the computer.
+        let items = ['Rock', 'Paper', 'Scissors'];
+        let compChoice = items[Math.floor(Math.random()* items.length)];
+        return compChoice; //return the selection of the computer.
     }
 //function that user plays with computer
 function playRound(playerSelection, computerSelection)
-    {   //if both select same, it is a tie
-        if(((playerSelection == 'PAPER') && (computerSelection =='PAPER'))||
-          ((playerSelection == 'ROCK') && (computerSelection =='ROCK'))||
-          ((playerSelection =='SCISSORS'))&& ((computerSelection == 'SCISSORS')))
-            {
-                result = "It's a Tie!";
-                return(" Your Score " +userScore+ " Computer Score " +compScore+"  "+ result);
-                
-            }
-        else if(((playerSelection == 'PAPER')&&(computerSelection =='ROCK'))||
-               ((playerSelection == 'ROCK')&&(computerSelection =='SCISSORS'))||
-               ((playerSelection == 'SCISSORS')&&(computerSelection =='PAPER')))
-
-            {
-                result = ` You Win!! ${playerSelection} beats ${computerSelection} `;
-                userScore += 1;
-                //compScore -= 1;
-                return(" Your Score " +userScore+ " Computer Score " +compScore+"  "+ result);
-                 
-            }
-        else if(((playerSelection =='ROCK') && (computerSelection == 'PAPER'))||
-               ((playerSelection == 'SCISSORS')&&(computerSelection =='ROCK'))||
-               ((playerSelection == 'PAPER')&&(computerSelection =='SCISSORS')))
-            {
-                result = ` You loose!! ${computerSelection} beats ${playerSelection} `;
-                compScore += 1;
-                //userScore -= 1;
-                return(" Your Score " +userScore+ " Computer Score " +compScore+"  "+ result);
-            }
-               
+    {   
+        if((playerSelection == 'PAPER')&&(computerSelection =='ROCK')||
+        (playerSelection == 'ROCK')&&(computerSelection =='SCISSORS')||
+        (playerSelection == 'SCISSORS')&&(computerSelection =='PAPER'))
+        {
+            result = ` You Win!! ${playerSelection} beats ${computerSelection} `;
+            userScore += 1;
+        //compScore -= 1;
+        return(" Your Score " +userScore+ " Computer Score " +compScore+"  "+ result);
+        }
+        else if((playerSelection =='ROCK') && (computerSelection == 'PAPER')||
+            (playerSelection == 'SCISSORS')&&(computerSelection =='ROCK')||
+            (playerSelection == 'PAPER')&&(computerSelection =='SCISSORS'))
+        {
+            result = ` You loose!! ${computerSelection} beats ${playerSelection} `;
+            compScore += 1;
+            //userScore -= 1;
+            return(" Your Score " +userScore+ " Computer Score " +compScore+"  "+ result);
+        }
+        else
+        {
+            //else it is a tie
+            result = "It's a Tie!";
+            return(" Your Score " +userScore+ " Computer Score " +compScore+"  "+ result);
+        } 
     }
-
+               
 function game()
     {
-    for(let i=0; i< 5; i++)
+        for(let i=0; i< 5; i++)
         {   
-        //calling function for computer to select
-        let computerSelection = computerPlay(); 
-        computerSelection = computerSelection.toUpperCase(); 
-        //asking user to select
-        let playerSelection = prompt('Type "Rock" or "Paper" or "Scissors"'); 
-        playerSelection = playerSelection.toUpperCase();
-        // calling the playRound to play
-        let play = playRound(playerSelection, computerSelection);
-        //console.log(play);
-        alert(play);
+            //calling function for computer to select
+            let computerSelection = computerPlay(); 
+            computerSelection = computerSelection.toUpperCase(); 
+            //asking user to select
+            let playerSelection = prompt('Type "Rock" or "Paper" or "Scissors"'); 
+            playerSelection = playerSelection.toUpperCase();
+            // calling the playRound to play
+            let play = playRound(playerSelection, computerSelection);
+            //console.log(play);
+            alert(play);
         }
-        //Adding total scores 
-        userScoretotal += userScore;
-        compScoretotal += compScore;
-        //comparing user score with computer score
-    if (userScoretotal > compScoretotal)
+        
+        if (userScore > compScore)
         {
-        //console.log(" You are the Winner! with total score: "+userScoretotal);
-        alert(" You are the Winner! with total score:  "+userScoretotal);
+            //console.log(" You are the Winner! with total score: "+userScoretotal);
+            alert(" You are the Winner! with total score:  "+userScore);
         }
-    else if(userScoretotal == compScoretotal)
+        else if(userScore == compScore)
         {
-        //console.log(" Computer is the Winner! with total score: "+compScoretotal);
-        alert(" Both of your scores are same!! Computer score: "+compScoretotal +" equals to Player score: "+ userScoretotal);
+            //console.log(" Computer is the Winner! with total score: "+compScoretotal);
+            alert(" Both of your scores are same!! Computer score: "+compScore +" equals to Player score: "+ userScore);
         }
-    else{
-        alert(" Computer is the Winner! with total score: " +compScoretotal);
+        else
+        {
+            alert(" Computer is the Winner! with total score: " +compScore);
         }    
     }
 //calling the game to paly 5 rounds    
